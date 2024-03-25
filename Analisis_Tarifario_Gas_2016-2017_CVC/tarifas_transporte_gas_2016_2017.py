88fe1154f6f4dce13ec53c849ea3a5b3fc6e1646
Jesus Santiago <Sanjesvel@outlook.com>
2024-03-24 23:46:35 -0600

    Funcion_Validacion_del_Archivo_CVS

--- Analisis_Tarifario_Gas_2016-2017_CVC/tarifas_transporte_gas_2016_2017.py
+++ Analisis_Tarifario_Gas_2016-2017_CVC/tarifas_transporte_gas_2016_2017.py
@@ -1,2 +1,37 @@
 #Created by:Jesus Santiago Velasco  20/03/2024
 #Email: Sanjesvel@outlook.com
+'''Librerias'''
+import pandas as pd
+import matplotlib.pyplot as plt
+import waterfall_chart
+
+'''Variables'''
+datos_cvs = None
+columnas_prom = []
+
+'''Funciones'''
+
+#Declaramos una funcion donde el usuario ingrese el la ruta del archivo csv
+def validar_archivo_csv():
+     while True:
+        ruta_archivo = input("Ingrese la ruta del archivo CSV: ")
+        if ruta_archivo: #Validamos si la ruta es valida
+            try: #validamos si es un archivo cvs
+                datos_cvs = pd.read_csv(ruta_archivo)
+                print("Archivo CSV seleccionado Correctamente \n")  
+                return datos_cvs
+            except Exception as e:
+                print("Error al abrir el archivo asegurese si es una archivo CVS", e)
+        else:
+            print("La ruta ingresada no es válida.")
+
+
+
+'''Codijo_Principal'''
+
+datos_cvs = validar_archivo_csv()
+
+
+
+
+
