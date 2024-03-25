d2eaba6be7eeba8a6c0737d3722ac4eeca2f47c2
Jesus Santiago <Sanjesvel@outlook.com>
2024-03-25 02:50:55 -0600

    Funcion_de_Selecion_columnas_y_zonas

--- Analisis_Tarifario_Gas_2016-2017_CVC/tarifas_transporte_gas_2016_2017.py
+++ Analisis_Tarifario_Gas_2016-2017_CVC/tarifas_transporte_gas_2016_2017.py
@@ -4,34 +4,85 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import waterfall_chart
-
+import os
 '''Variables'''
-datos_cvs = None
-columnas_prom = []
-
+datos_csv = None
+columnas_selec= []
+zona_A_analizar = None
 '''Funciones'''
-
 #Declaramos una funcion donde el usuario ingrese el la ruta del archivo csv
 def validar_archivo_csv():
-     while True:
+    while True:
         ruta_archivo = input("Ingrese la ruta del archivo CSV: ")
-        if ruta_archivo: #Validamos si la ruta es valida
-            try: #validamos si es un archivo cvs
-                datos_cvs = pd.read_csv(ruta_archivo)
+        if ruta_archivo: # Validamos si la ruta es valida
+            try: # validamos si es un archivo cvs
+                datos_csv = pd.read_csv(ruta_archivo)
                 print("Archivo CSV seleccionado Correctamente \n")  
-                return datos_cvs
+                return datos_csv
             except Exception as e:
-                print("Error al abrir el archivo asegurese si es una archivo CVS", e)
+                print("Error al abrir el archivo asegurese si es una archivo CSV", e)
         else:
             print("La ruta ingresada no es válida.")
 
 
-
+# Declaramos la funcion para Selecionar la zona y columnas del Archivo CVS para analizar
+def seleccion_zona_y_columnas(datos_csv):
+    columnas_a_analizar = []
+    while True: # bucle por si el usuario selecione una zona invalida 
+        zonas = datos_csv["zona"].unique() 
+        print("Zonas del archivo CSV:") 
+        for i in range(len(zonas)): # Muestra las zonas disponibles
+            print(f"{i+1}.{zonas[i]}")
+        
+        selec_zona = input("\nPor favor, Ingresa el nombre de la zona que desea analizar: ")
+        if selec_zona in zonas: # Verifica si la zona eliguida es valida 
+            print(f"\nHas seleccionado la zona '{selec_zona}' para analizar.")    
+            # Selección de columnas
+            while True: # bucle por si el usuario selecione una columna o una cantidad de columnas a analizar invalida
+                columnas = datos_csv.select_dtypes(include='float').columns
+                print("\nColumnas disponibles a analizar:")
+                for i in range(len(columnas)): # Muestra las columnas Disponibles
+                    print(f"{i+1}.{columnas[i]}")
+                
+                # El try son por el motivo si el usario mete un dato erronio pueda continuar el progrmas sin fallar
+                num_columnas = input("\nPor favor, ingrese cuantas columnas desea analizar: ")
+                try: # Verifica si el dato es entero
+                    num_columnas = int(num_columnas)
+                except ValueError:
+                    os.system('clear')
+                    print("Error: Debe ingresar un número entero.")
+                    continue
+                # Verifica si el numero de columnas para analisar esta dentro del rango invalido
+                if num_columnas <= 0 or num_columnas > len(columnas): 
+                    os.system('clear') # Limpia la consola
+                    print("Error: El numero de columnas a analizar no es valido.")
+                print("\nPor favor, ingrese el nombre de las columnas a analizar:")
+                for k in range(num_columnas): #
+                    while True:
+                        print("\nColumnas disponibles a analizar:")
+                        for i in range(len(columnas)): # Muestra las columnas disponibles para seleccionar
+                            print(f"{i+1}.{columnas[i]}")
+                       
+                        nombre_columna = input(f"\ningrese el nombre de la columna {k+1} eligida a analizar: ")
+                        if nombre_columna in columnas: # Verifica si la columna ingresada es valida
+                            columnas_a_analizar.append(nombre_columna)
+                            break
+                        else:
+                            os.system('clear') # Limpia la consola
+                            print(f"Error: La columna '{nombre_columna}' no existe en el archivo CSV.") 
+                                          
+                break  # Salir del bucle while de selección de columnas
+            
+            return selec_zona, columnas_a_analizar
+        else:
+            os.system('clear') # Limpia la consola
+            print("\nLa zona seleccionada no es válida. Por favor, intente nuevamente.\n")
+  
+    
 '''Codijo_Principal'''
 
-datos_cvs = validar_archivo_csv()
+datos_csv = validar_archivo_csv()
 
-
-
+zona_A_analizar , columnas_selec =seleccion_zona_y_columnas(datos_csv)
 
 
