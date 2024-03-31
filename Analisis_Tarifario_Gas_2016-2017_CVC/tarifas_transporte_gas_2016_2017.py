930e846e7d88752a17f64501d02559522bdbee2c
Jesus Santiago <Sanjesvel@outlook.com>
2024-03-31 09:03:12 -0600

    Funcion_graficar_y_correguir_codijo

--- Analisis_Tarifario_Gas_2016-2017_CVC/tarifas_transporte_gas_2016_2017.py
+++ Analisis_Tarifario_Gas_2016-2017_CVC/tarifas_transporte_gas_2016_2017.py
@@ -27,7 +27,7 @@ def validar_archivo_csv():
 # Declaramos la funcion para Selecionar la zona y columnas del Archivo CVS para analizar
 def seleccion_zona_y_columnas(datos_csv):
     '''Variables'''
-    columnas_a_analizar = []
+    columnas_a_analizar =[]
     zonas = None
     selec_zona = None
     columnas = None
@@ -92,7 +92,7 @@ def mostrar_menu():
     print("3. Demanda promedio del gas natural para 2016")
     print("4. Demanda promedio del gas natural para 2017")
     print("5. Tarifas máximas por año")
-    print("7. Salir")
+    print("6. Salir")
  
   
 #Declaramos la funcion para validar datos(Columnas a promediar y promediar las columnas de datos tarifarios
@@ -106,7 +106,10 @@ def promedio_costo_gas_2016_2017(datos_csv):
     while iterador1 < len(columnas_selec) : #while donde sacamos el promedio con el metodo mean
         promedio_columnas.append(zona_A_analizar[columnas_selec[iterador1]].mean())
         iterador1 +=1
-        
+    #graficamos los datos obtenidos 
+    graficar_barras(f"Promedio del costo del transporte del gas 2016-2017 de la zona{zona_A_analizar["zona"].unique()} ","Columnas analizadas del archivo csv","Promedio de Pesos por Gigajoule",columnas_selec,promedio_columnas)
+    
+     
             
 #Declaramos Fucion que calcule la variacion porcentual del precio de gas alo largo de los 2 años
 def variacion_porcentual_precio_gas_2016_2017(datos_csv):
@@ -119,7 +122,9 @@ def variacion_porcentual_precio_gas_2016_2017(datos_csv):
     while iterador1 < len(columnas_selec_variacion):# while Precios del gas natural al inicio y al final de los dos años de una misma columna donde lo multiplacamos por 100 para tener el porcentaje de la variacion 
         variacion_porcentual_columnas.append( ((zona_A_analizar[columnas_selec_variacion[iterador1]].iloc[-1] - zona_A_analizar[columnas_selec_variacion[iterador1]].iloc[0]) / zona_A_analizar[columnas_selec_variacion[iterador1]].iloc[0])* 100 ) 
         iterador1 += 1
-    
+    #graficamos los datos obtenidos 
+    graficar_barras(f"Variacion del costo del transporte del gas 2016-2017 de la zona {zona_A_analizar["zona"].unique()}","Columnas analizadas del archivo csv","Variacion porcentual entre 2016-2017",columnas_selec_variacion,variacion_porcentual_columnas)
+  
 #Declaramos la funcion para la demanda promedio pues es la unica que es posible calcular con esste archivo del precio del gas año 2016
 
 def demanda_promedio_gas(datos_csv,fecha):
@@ -140,12 +145,50 @@ def demanda_promedio_gas(datos_csv,fecha):
         demanda_total = zona_A_analizar[columnas_selec[iterador1]].sum()
         demanda_promedio.append((demanda_total / dias_del_ano)* 100)
         iterador1 += 1 
+    #graficamos los datos obtenidos    
+    graficar_barras(f"Demanda del costo del transporte del gas {fecha} de la zona {zona_A_analizar["zona"].unique()}","Columnas analizadas del archivo csv",f"Demanda promedio del año {fecha}",columnas_selec,demanda_promedio)
     
-
-   
+def tarifas_maximas_por_ano(datos_csv,anos):
+    '''Variables'''
+    zona_A_analizar = None
+    columnas_selec=[]
+    iterador1=0
+    valores_maximos= []
+    # Convertir la columna de fechas a tipo datetime si no está en ese formato
+    datos_csv['fecha_inicio'] = pd.to_datetime(datos_csv['fecha_inicio'])
+    # Filtrar los datos por año
+    for ano in anos:
+        datos_ano = datos_csv[datos_csv['fecha_inicio'].dt.year == ano]  #filtramos datos por el año
+        print(f"Seleccione los datos de los que desea sacar el máximo histórico del año {ano}")
+        zona_A_analizar, columnas_selec = seleccion_zona_y_columnas(datos_ano)
+        iterador1=0 #limpiamos el iterador
+        while iterador1 < len(columnas_selec) :#while donde seleciona los valores maximos de cada columna
+             valores_maximos.append(zona_A_analizar[columnas_selec[iterador1]].max())
+             iterador1 += 1
+        #graficamos los valores maximos de cada año
+        graficar_barras(f"\nValores maximos del costo del gas {ano} de la zona {zona_A_analizar["zona"].unique()}","Columnas analizadas del archivo csv",f"valores maximos del {ano}",columnas_selec,valores_maximos)
+        valores_maximos.clear()#Limpiamos el array para obtener los proximos valores maximos del otro años
     
-
-
+#funcion que pregunta al usuario si desea graficar los datos
+def preguntar_graficar():
+    respuesta = input("¿Deseas graficar los datos? (s/n): ")
+    return respuesta.lower() == 's'
+           
+#funcion para graficar los datos que queremos 
+def graficar_barras(titulografic,nombre_eje_x,nombre_eje_y,columnas_selec,valores_a_graficar):
+    if preguntar_graficar():
+       fig ,ax = plt.subplots(figsize=(20, 8))  # Crear la figura y los ejes 
+       """Creamos Las Graficas En forma de Barras de las Zonas """
+       plt.suptitle(f"{titulografic} ", fontsize=16, fontweight="bold") #Titulo de la hoja de las Graficas
+       """Graficamos Las 2 Primeras Columnas de la zona"""
+       ax.bar(columnas_selec,valores_a_graficar,  color = 'tab:purple' )  #Graficamos el Precio por capacidad base firme ("Asignamos el rango Y Enbellecimietno ")
+       ax.set_ylabel(f"{nombre_eje_y}" , ) #Asignamos Nombre al Eje y
+       ax.set_title(f'{titulografic}', loc = "left", fontdict = {'fontsize':14, 'fontweight':'bold', 'color':'tab:purple'}) #Asignamos un Titulo Ala grafica
+       ax.set_xticklabels(columnas_selec,rotation=10, fontdict = { 'fontsize':8,'fontweight':'bold','color':'tab:blue'})  # Acomodamos los rangos del eje x s
+       ax.set_xlabel(f"{nombre_eje_x}",rotation = 0 ,fontdict = { 'fontsize':14, 'fontweight':'bold', 'color':'tab:purple'}) #Asignamos Titulo al Eje X 
+       plt.show()
+    else:
+        print("Nose grafico ningun datos")
 
 '''Codijo_Principal'''
 print("Bienvenidos AL Programa Para Analizar Su Archivo CSV")
@@ -161,11 +204,10 @@ while True:
            variacion_porcentual_precio_gas_2016_2017(datos_csv)
         elif opcion == "3":
            demanda_promedio_gas(datos_csv,2016)
-        
         elif opcion == "4":
            demanda_promedio_gas(datos_csv,2017)
-        #elif opcion == "5":
-           tarifas_maximas_por_ano()
+        elif opcion == "5":
+           tarifas_maximas_por_ano(datos_csv,[2016,2017])
         elif opcion == "6":
             print("Saliendo del programa...")
             break
